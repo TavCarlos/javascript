cbfa86a51ee3b38c9e392840dbbda528b5f58a5d
Antonio Carlos Tavares <123905174+TavCarlos@users.noreply.github.com>
2023-03-01 19:44:23 -0300

    Update

---
+++ curso_js/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel} KM/H`)
+
+if (vel > 60) { //Condição Simples
+    console.log(`você ultrapassou a velocidade permitida. Multado!`)
+}
+
+console.log("Dirija sempre utiliando cinto de segurança")
---
+++ curso_js/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'eua'
+
+if (pais == 'brasil'){
+    console.log('Brasileiro')
+} else{
+    console.log('Estrangeiro')
+}
