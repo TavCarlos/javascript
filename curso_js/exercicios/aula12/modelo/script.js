42b6fe7b8eee01df2feba3ce6741c807c7a4c221
Antonio Carlos Tavares <123905174+TavCarlos@users.noreply.github.com>
2023-03-06 20:40:37 -0300

    Update

--- curso_js/exercicios/aula12/modelo/script.js
+++ curso_js/exercicios/aula12/modelo/script.js
@@ -1,5 +1,6 @@
 var data = new Date()
 var hora = data.getHours()
+var minuto = data.getMinutes()
 
 
 window.addEventListener('DOMContentLoaded', loading) //aciona a função ao atualizar da página
@@ -8,7 +9,20 @@ window.addEventListener('DOMContentLoaded', loading) //aciona a função ao atua
 
 function loading(){
     var msg = window.document.querySelector('div#msg')
-    var img = window.document.querySelector ('div#img')
+    var img = window.document.querySelector ('img#imagem')
+   
     
-    msg.innerHTML = `Agora são ${hora} horas!`
+    msg.innerHTML = `Agora são ${hora} horas e ${minuto} minutos!`
+    
+    if (hora >= 0 && hora < 12){
+        img.src = '../imagem/foto-manha.jpg' //Troco a imagem da div > img
+        document.body.style.background = '#C057DE'
+
+    } else if (hora >= 12 && hora < 18){
+        img.src = '../imagem/foto-tarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else {
+        img.src = '../imagem/foto-noite.jpg'
+        document.body.style.background = '#515154'//muda a background color do body
+    }
 }
