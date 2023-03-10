deb9a2961a01dbeb774d237a6807690d3ce9a024
Antonio Carlos Tavares <123905174+TavCarlos@users.noreply.github.com>
2023-03-10 20:09:43 -0300

    update

--- curso_js/exercicios/aula14/ex002/modelo/javascript.js
+++ curso_js/exercicios/aula14/ex002/modelo/javascript.js
@@ -8,18 +8,25 @@ function tabuada(){
     }else{
         var numero = Number(txt.value)
         var p = document.createElement("p")
-        p.setAttribute('id', 'tabuada')
+        //p.style.textAlign = "left"
+        var operador = window.document.getElementsByName('tabuada')
         var res = document.getElementsByTagName('p')[0]
-        res.innerHTML = `Multiplicação de: ${numero}`
 
-        for (var contador = 1; contador <= 10; contador++ ){ 
-            p.innerHTML += `${contador} x ${numero} = ${contador*numero} `
+        if (operador[0].checked){
+            res.innerHTML = `Multiplicação de: ${numero}`
+
+            for (var contador = 1; contador <= 10; contador++ ){ 
+                p.innerHTML += `${contador} x ${numero} = ${contador*numero} <br>`
+            }
+        
+        } else{
+            res.innerHTML = `Divisão de: ${numero}`
+
+            for (var contador = 1; contador <=10; contador++){
+                var multi = contador*numero
+                p.innerHTML += `${multi} / ${numero} = ${contador} <br>`
+            }
         }
-        res.appendChild(p)
+        res.appendChild(p) 
     }
 }
-
-
-
-//var res = document.getElementsByTagName('p')[0]
-//res.innerHTML = ` ${numero}`
